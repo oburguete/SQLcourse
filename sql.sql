bd139d80a702bd0bd10cedfa3ec9f4d0f0676477
oscar <oscarburguete@gmail.com>
2023-03-09 18:21:40 -0600

    se agregaron case y otras cosas

--- sql.sql
+++ sql.sql
@@ -42,3 +42,68 @@ FROM employees
  
  SELECT officeCode, employeeNumber, lastName, firstName FROM  employees
  WHERE reportsTo is NULL;
+ 
+ SELECT  *
+ FROM employees
+ ORDER BY lastName;
+ 
+ SELECT *
+ FROM employees
+ WHERE jobTitle = 'sales rep'
+ ORDER BY employeeNumber;
+ 
+SELECT  officeCode AS 
+FROM employees;
+
+SELECT COUNT(DISTINCT officeCode) FROM employees;
+
+SELECT officeCode, COUNT(employeeNumber)
+FROM employees
+GROUP BY officeCode;
+
+SELECT *
+FROM employees
+ORDER BY officeCode;
+
+SELECT jobTitle, COUNT(jobTitle) AS 'cantidad'
+FROM employees
+GROUP BY jobTitle
+ORDER BY cantidad DESC;
+
+SELECT reportsTO, COUNT(reportsTO)
+FROM employees
+group by reportsTO;
+ 
+SELECT round(8.5);
+
+SELECT UPPER(lastName)
+FROM employees;
+
+SELECT CONCAT(UPPER(lastName),' ',UPPER(firstName))
+FROM employees;
+SELECT LTRIM('     5 ESPACIOS');
+
+SELECT NOW();
+SELECT CURRENT_DATE(), CURDATE();
+
+SELECT CURRENT_TIME();
+
+SELECT firstName, 
+CASE officeCode
+WHEN 1 THEN 'PISO 1'
+WHEN 2 THEN 'PISO 2'
+WHEN 3 THEN 'PISO 3'
+ELSE 'PLANTA BAJA'
+END AS 'UBICACION'
+FROM employees;
+
+SELECT PRODUCTS;
+
+
+SELECT productName,
+	CASE WHEN quantityInStock < 1000  THEN 'urge realizar pedido'
+	WHEN quantityInStock > 1000 AND quantityInStock < 2000 THEN 'REALIZAR PEDIDO ESTA SEMANA'
+	ELSE 'en stock'
+	END
+FROM PRODUCTS 
+ORDER BY quantityInStock;
